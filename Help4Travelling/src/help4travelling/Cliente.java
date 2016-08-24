2c71ab7d3130c533971ea9d0b41c43f4f8d75338
bruno-full-name <bruno.carballido@gmail.com>
2016-08-24 19:07:30 -0300

    Codigo en las clases Usuario, Cliente, proveedor, DtCliente, DtProveedor

--- Help4Travelling/src/help4travelling/Cliente.java
+++ Help4Travelling/src/help4travelling/Cliente.java
@@ -1,6 +1,35 @@
-
 package help4travelling;
 
-public class Cliente {
+/**
+ *
+ * @author Bruno
+ */
+public class Cliente extends Usuario{
+    
+    public Cliente (DtCliente u){
+        this.nick= u.getNick();
+        this.nombre= u.getNombre();
+        this.apellido= u.getApellido();
+        this.email= u.getEmail();
+        this.fechaN= u.getFechaN();
+        this.avatar= u.getAvatar();        
+    }
+    
+    
+    @Override
+    public String getNickCliente(){
+        return this.nick;
+    }
+    
+    @Override
+    public String getNickProveedor(){
+        return "";
+    }
+    
+    
+    //falta lista de reservas
+    public DtCliente getDtCliente(){
+        return new DtCliente(nick, nombre, apellido, email, fechaN, avatar);
+    }
     
 }
