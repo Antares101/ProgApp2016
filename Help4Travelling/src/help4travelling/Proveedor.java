fd2f948dc9e67af832ea6ed73a57875ba995c58a
bruno-full-name <bruno.carballido@gmail.com>
2016-08-24 18:59:29 -0300

    Codigo en las clases Usuario, Cliente, proveedor, DtCliente, DtProveedor

--- Help4Travelling/src/help4travelling/Proveedor.java
+++ Help4Travelling/src/help4travelling/Proveedor.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package help4travelling;
 
 /**
  *
- * @author agustin
+ * @author Bruno
  */
-public class Proveedor {
+public class Proveedor extends Usuario{
     
+    private String nombreEmpresa;
+    private String url;
+    
+    public Proveedor (DtProveedor u){
+        this.nick= u.getNick();
+        this.nombre= u.getNombre();
+        this.apellido= u.getApellido();
+        this.email= u.getEmail();
+        this.fechaN= u.getFechaN();
+        this.avatar= u.getAvatar(); 
+        this.nombreEmpresa= u.getNombreEmpresa();
+        this.url= u.getUrl();
+    }
+    
+    
+    @Override
+    public String getNickCliente(){
+        return "";
+    }
+    
+    @Override
+    public String getNickProveedor(){
+        return this.nick;
+    }
+    
+    //falta lista de servicios
+    public DtProveedor getDtProveedor(){
+        return new DtProveedor(nick, nombre, apellido, email, fechaN, avatar, nombreEmpresa, url);
+    }
 }
