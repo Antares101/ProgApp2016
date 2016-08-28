b09386e1976cec93e95a5e0b2154bd218e956b14
Nachomax <josigarcia94@hotmail.com>
2016-08-28 14:04:35 -0300

    Update Categoria.java

--- Help4Travelling/src/help4travelling/Categoria.java
+++ Help4Travelling/src/help4travelling/Categoria.java
@@ -1,28 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package help4travelling;
 
+import java.util.HashSet;
 import java.util.HashMap;
 import java.util.ArrayList;
-
 /**
  *
- * @author Bruno
+ * @author Nacho
  */
 public class Categoria {
-  
-      private String nombre;
-      
-      private HashMap<String, Servicio> servicios = new HashMap<String, Servicio>();
-      
-      public String getNombre(){
-        return this.nombre;
-      }
-      
-      public ArrayList<String> listarServicios(){
-        ArrayList<String> ArrayServicios = new ArrayList<String>();
-        for (String name: servicios.keySet()) {
-            ArrayServicios.add(servicios.get(name).GetNombre());
-        }
-        return ArrayServicios;
-      }
+    String Nombre;
+    private HashSet<Categoria> cathijas;
+    DtCategoria DtCat;
+    private HashMap<String, Servicio> servicios = new HashMap<String, Servicio>();
+    
+    //CONSTRUCTOR
+    public Categoria(String Nombre){
+        this.Nombre = Nombre;
+        cathijas = new HashSet<Categoria>();
+    }
+    
+    
+    public String getNombre(){
+        return this.Nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.Nombre=nombre;
+    }
+    
+    public void AgregarCatHija(Categoria hija){
+        cathijas.add(hija);
+    }
+    
+    public DtCategoria GetDtCategoria(){
+        return this.DtCat;
+    }
+    
+    public ArrayList<String> listarServicios(){
+       ArrayList<String> ArrayServicios = new ArrayList<String>();
+       for (String name: servicios.keySet()) {
+           ArrayServicios.add(servicios.get(name).GetNombre());
+       }
+       return ArrayServicios;
+    }
+
+    
     
 }
+
