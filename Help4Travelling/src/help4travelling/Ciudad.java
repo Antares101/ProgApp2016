a38734b1613adec950878a5ffb44a25604c00f11
bruno-full-name <bruno.carballido@gmail.com>
2016-08-27 23:10:56 -0300

    DTServicio

--- Help4Travelling/src/help4travelling/Ciudad.java
+++ Help4Travelling/src/help4travelling/Ciudad.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package help4travelling;
 
 /**
@@ -10,5 +5,11 @@ package help4travelling;
  * @author agustin
  */
 public class Ciudad {
+    
+    private String nombre;
+    
+    public String getNombre(){
+        return this.nombre;
+    }
     
 }
--- Help4Travelling/src/help4travelling/Servicio.java
+++ Help4Travelling/src/help4travelling/Servicio.java
@@ -1,5 +1,8 @@
 package help4travelling;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+
 import java.sql.Blob;
 
 /*
@@ -10,10 +13,19 @@ public class Servicio extends Articulo{
     private Blob[] image;
     private String descripcion;
     private Float precio;
-    //private Categoria[] cat;  //Crear clase categoria
+    private HashMap<String, Categoria> categorias = new HashMap<String, Categoria>();
+    private Ciudad ciudadOrigen;
+    private Ciudad ciudadDestino;
     //private Promocion[] prom;
     
     public void AddImage(Blob Image){}
     
+    public DtServicio getDtServicio(){
+        ArrayList<String> ArrayCategorias = new ArrayList<String>();
+        for (String name: categorias.keySet()) {
+            ArrayCategorias.add(categorias.get(name).getNombre());
+        }
+        return new DtServicio(nombre, precio,image, descripcion, ArrayCategorias, ciudadOrigen.getNombre(), ciudadDestino.getNombre());
+    }
     
 }
